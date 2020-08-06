9607529102e460ce5b1e6aa1fac963a27cea2a65
Tamasi Julia <juliatamasi33@gmail.com>
2020-08-05 22:43:07 -0300

    Clase 12

--- 9-Parciales/2020_1Cuatri_parcial_fuentes/dos.js
+++ 9-Parciales/2020_1Cuatri_parcial_fuentes/dos.js
@@ -1,4 +1,186 @@
+/*Realizar el algoritmo que permita ingresar los datos de una compra productos de la construccion, hasta que el cliente quiera:
+Tipo validad("arena";"cal";"cemento")
+Cantidad de bolsas,
+Precio por bolsa (más de cero ),
+
+Si compro más de 10 bolsas en total tenes 15% de descuento sobre el total a pagar.
+Si compro más de 30 bolsas en total tenes 25% de descuento sobre el total a pagar.
+a) El importe total a pagar , bruto sin descuento y...
+b) el importe total a pagar con descuento(solo si corresponde)
+d) Informar el tipo con mas cantidad de bolsas.
+f) El tipo mas caro
+*/
+
 function mostrar()
 {
-  alert("dos");
+  //declaracion de variables
+
+  var tipoBolsa;
+  var cantidadBolsas;
+  var precioBolsa;
+  var respuesta;
+  var descuento;
+
+
+  var importeBruto;
+  var importeDescuento;
+  var bolsasArena;
+  var bolsasCal;
+  var bolsasCemento;
+  var precioArena;
+  var precioCal;
+  var precioCemento;
+  var precioBolsaTotal;
+  var cantidadBolsasTotal;
+  var maximaCantidadBolsas;
+  var tipoMasCaro;
+  
+
+  //inicializacion de contadores
+
+  respuesta = true;
+  descuento = 0;
+  importeBruto = 0;
+  precioArena = 0;
+  precioCal = 0;
+  precioCemento = 0;
+  precioBolsaTotal = 0;
+  bolsasCemento = 0;
+  bolsasCal = 0;
+  bolsasArena = 0;
+  
+  
+  while (respuesta == true)
+  {
+    //validacion de datos
+    
+    tipoBolsa = prompt("Ingrese el tipo de producto que desea adquirir");
+    
+    while (tipoBolsa != "arena" && tipoBolsa != "cemento" && tipoBolsa != "cal")
+    {
+      tipoBolsa = prompt("ERROR. Reingrese el tipo de producto (arena, cemento o cal)");
+    }
+
+    cantidadBolsas = prompt("Ingrese la cantidad de bolsas que desea");
+    cantidadBolsas = parseInt(cantidadBolsas);
+
+    while(cantidadBolsas<1 || isNaN(cantidadBolsas) == true)
+    {
+      cantidadBolsas = prompt("ERROR. Reingrese la cantidad de bolsas que desea");
+      cantidadBolsas = parseInt(cantidadBolsas);
+    }
+
+    precioBolsa = prompt("Ingrese el precio por bolsa");
+    precioBolsa = parseInt(precioBolsa);
+    
+    while(precioBolsa<0)
+    {
+      precioBolsa = prompt("ERROR. Reingrese el precio por bolsa");
+      precioBolsa = parseInt(precioBolsa);
+    }
+
+    
+    //separo por tipo
+    
+    switch (tipoBolsa)
+    {
+      case "cemento" :
+        bolsasCemento += cantidadBolsas;
+        precioCemento += precioBolsa*cantidadBolsas;
+        break;
+      case "arena" : 
+        bolsasArena += cantidadBolsas;
+        precioArena += precioBolsa*cantidadBolsas;
+        break;
+      case "cal" :
+        bolsasCal += cantidadBolsas;
+        precioCal += precioBolsa*cantidadBolsas;
+        break;
+    }
+    
+    respuesta = confirm("¿Desea ingresar otra compra?");   
+
+  }
+
+  //calculo el tipo con mas bolsas
+
+  if(bolsasCemento > bolsasArena && bolsasCemento > bolsasCal)
+    {
+      maximaCantidadBolsas = "cemento";
+    }
+    else
+    {
+      if (bolsasArena > bolsasCemento && bolsasArena > bolsasCal)
+      {
+        maximaCantidadBolsas = "arena";
+      }
+      else
+      {
+        maximaCantidadBolsas = "cal";
+      }
+    }
+
+  //calculo el tipo mas caro
+
+  if(precioArena > precioCal && precioArena > precioCemento)
+  {
+    tipoMasCaro = "arena";
+  }
+  else
+  {
+    if(precioCal > precioCemento && precioCal > precioArena)
+    {
+      tipoMasCaro = "cal";
+    }
+    else
+    {
+      tipoMasCaro = "cemento";
+    }
+  }
+
+  //calculo los totales
+
+  cantidadBolsasTotal = bolsasArena + bolsasCal + bolsasCemento;
+  precioBolsaTotal = precioArena + precioCal + precioCemento;
+  
+  //seteo el descuento
+  
+  if(cantidadBolsasTotal > 29)
+  {
+    descuento = precioBolsaTotal*0.25;
+  }
+  else
+  {
+    if (cantidadBolsasTotal > 9)
+    {
+      descuento = precioBolsaTotal*0.15;
+    }
+  }
+  
+  //calculo el importe total
+
+  importeBruto = precioBolsaTotal;
+  importeDescuento =  precioBolsaTotal - descuento;
+
+  //muestro por consola
+
+  console.log(`el importe bruto es de ${importeBruto}`);
+  console.log(`el importe con descuento es de ${importeDescuento}`);
+  console.log(`el tipo mas caro es ${tipoMasCaro}`);
+  console.log(`el tipo con mas bolsas es ${maximaCantidadBolsas}`);
+
 }
+
+
+
+
+  
+
+
+
+
+
+
+
+
+
--- 9-Parciales/2020_1Cuatri_parcial_fuentes/tres.js
+++ 9-Parciales/2020_1Cuatri_parcial_fuentes/tres.js
@@ -1,4 +1,124 @@
 function mostrar()
 {
-	alert("tres");
+	var nombre;
+	var edad;
+	var sexo;
+	var estadoCivil;
+	var temperaturaCorporal;
+	var respuesta;
+	var personaMasTemperatura;
+	var acumuladorTemperatura;
+	var acumuladorViudos;
+	var acumuladorHombresSolteros_Viudos;
+	var edadHombresSolteros;
+	var promedioHombresSolteros;
+	var hombresSolteros;
+	var terceraEdadAltaTemperatura;
+
+
+	// punto A
+	acumuladorTemperatura = 0;
+	
+	//punto B
+	acumuladorViudos = 0;
+
+	//punto C
+	acumuladorHombresSolteros_Viudos = 0;
+
+	//punto D
+	terceraEdadAltaTemperatura = 0;
+	
+	//punto E
+	edadHombresSolteros = 0;
+	hombresSolteros = 0;
+
+
+	respuesta = true;
+
+	while (respuesta == true)
+	{
+		//entrada y validacion de datos
+
+		nombre = prompt("Ingrese su nombre");
+		
+		edad = prompt("Ingrese su edad");
+		edad = parseInt(edad);
+		while(isNaN(edad)==true)
+		{
+			edad = prompt("ERROR. Reingrese su edad");
+			edad = parseInt(edad);
+		}
+
+		sexo = prompt("Ingrese su sexo (f/m)");
+		while(sexo != "f" && sexo != "m")
+		{
+			sexo = prompt("ERROR. Reingrese su sexo (f/m)");
+		}
+
+		estadoCivil = prompt("Ingrese su estado civil");
+		while(estadoCivil != "soltero" && estadoCivil != "casado" && estadoCivil != "viudo")
+		{
+			estadoCivil = prompt("ERROR.Reingrese un estado civil valido (soltero, casado, viudo)");
+		}
+
+		temperaturaCorporal = prompt("Ingrese su temperatura corporal");
+		temperaturaCorporal = parseInt(temperaturaCorporal);
+		while(isNaN(temperaturaCorporal)==true)
+		{
+			temperaturaCorporal = prompt("ERROR.Reingrese su temperatura corporal");
+			temperaturaCorporal = parseInt(temperaturaCorporal);
+		}
+
+		//punto A - Persona con mas temperatura
+		if(temperaturaCorporal>acumuladorTemperatura)
+		{
+			acumuladorTemperatura = temperaturaCorporal;
+			personaMasTemperatura = nombre;
+		}
+
+		//casos de estado civil
+		switch (estadoCivil)
+		{
+			case "viudo" :
+				if (edad>17)
+				{
+					acumuladorViudos++;
+				}
+				if (sexo == "m")
+				{
+					acumuladorHombresSolteros_Viudos++;
+				}
+				break;
+			case "soltero":
+				if (sexo == "m")
+				{
+					acumuladorHombresSolteros_Viudos++;
+					hombresSolteros++;
+					edadHombresSolteros += edad;
+				}
+				break;
+		}
+
+		//Punto D - Personas de la 3ra edad con alta temperatura
+
+		if(edad>59 && temperaturaCorporal>37.9)
+		{
+			terceraEdadAltaTemperatura++;
+		}
+
+		respuesta = confirm ("Desea cargar otro pasajero?")
+	}
+
+	//calculo el promedio. Punto E
+	
+	promedioHombresSolteros = edadHombresSolteros/hombresSolteros;
+
+	//muestro por consola
+	
+	console.log("La persona con mas temperatura es " + personaMasTemperatura);
+	console.log("Hay " + acumuladorViudos + " mayores de edad que estan viudos");
+	console.log("Hay " + acumuladorHombresSolteros_Viudos + " hombres que estan solteros o viudos");
+	console.log("Hay " + terceraEdadAltaTemperatura + " personas de la tercera edad que tienen mas de 38 de temperatura" );
+	console.log("El promedio de edad entre los hombres solteros es de " + promedioHombresSolteros);
+
 }
